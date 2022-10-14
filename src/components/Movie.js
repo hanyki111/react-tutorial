e628f7f1d564bd7d178c65c7df2474a5a8ce314a
hanyki111 <hanyki111@gmail.com>
2022-10-14 22:50:35 +0900

    1014

---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Movie({ id, cover_image, title, summary, genres }) {
+  return (
+    <div>
+      <img src={cover_image} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  cover_image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
